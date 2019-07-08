22176366818a1c9f4d0715009bf411a7092a76da
dggrom <dg-grom@mail.ru>
2019-07-09 00:06:32 +0300

    Finish View Coming consts direktory

--- src/Sempel/FormDirectory/DirectoruViewComingCosts/ControllerViewComongConsts.java
+++ src/Sempel/FormDirectory/DirectoruViewComingCosts/ControllerViewComongConsts.java
@@ -1,21 +1,29 @@
 package Sempel.FormDirectory.DirectoruViewComingCosts;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
+import Sempel.FormDirectory.DirectoruViewComingCosts.CreatUpdateViewComingCosts.ContCreatUpdateViewComCon;
+import Sempel.MainForm.ControllerMainForm;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import sql.SelectPost;
 import sql.SettingConnectSQL;
+
+import javax.swing.*;
 
 public class ControllerViewComongConsts {
 
@@ -58,6 +66,109 @@ public class ControllerViewComongConsts {
         TableColumnsDeleted.setCellValueFactory(new PropertyValueFactory<PersenViewComCons, Boolean>("Deleted"));
 
         TableViewComCon.setItems(PersenViewComCon);
+
+        ButtonCreat.setOnAction(event -> {
+            try {
+                OpenFormCreatUpdate(false);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+
+        ButtonRefresh.setOnAction(event -> {
+            try {
+                OpenFormCreatUpdate(true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+
+        ButtonDeleted.setOnAction(event -> {
+
+            PersenViewComCons PerView = TableViewComCon.getSelectionModel().getSelectedItem();
+            boolean delRez = DeletedView();
+
+            if (delRez){
+                try {
+                    DeletedBaseView(PerView.getDeleted(),PerView.getId().toString());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void DeletedBaseView(boolean Del, String intStr) throws SQLException {
+        String sqlText = "";
+        SelectPost SelPost = new SelectPost();
+        if (Del){
+             sqlText = "UPDATE public.\"ViewComingConsumption\" " +
+                    " SET deleted_viewcc = false " +
+                    " WHERE id_viewcc="+intStr+" ;";
+        } else {
+             sqlText = "UPDATE public.\"ViewComingConsumption\" " +
+                    " SET deleted_viewcc = true " +
+                    " WHERE id_viewcc="+intStr+" ;";
+        }
+        boolean RezUp = SelPost.UpdateCreatTable(SetCon.CreatConnect(),sqlText);
+        if (RezUp){
+            JOptionPane.showMessageDialog(null,"Error update","ERROR",JOptionPane.ERROR_MESSAGE);
+        } else {
+            refrashTableView();
+        }
+    }
+
+    private boolean DeletedView(){
+
+        PersenViewComCons PerCon = TableViewComCon.getSelectionModel().getSelectedItem();
+
+        Alert alertForm = new Alert(Alert.AlertType.CONFIRMATION);
+        alertForm.setTitle("DELETED");
+        alertForm.setHeaderText("Deleted/not deleted " + PerCon.getView() + " ?");
+        Optional<ButtonType> optt = alertForm.showAndWait();
+
+        if (optt.get() == ButtonType.OK){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void OpenFormCreatUpdate(Boolean updatedata) throws IOException, SQLException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ControllerMainForm.class.getResource("/Sempel/FormDirectory/DirectoruViewComingCosts/CreatUpdateViewComingCosts/FornCreatUpdateViewComCon.fxml"));
+
+        PersenViewComCons PerVCC = TableViewComCon.getSelectionModel().getSelectedItem();
+
+        if (updatedata) {
+            ContCreatUpdateViewComCon ControllerUpCr = new ContCreatUpdateViewComCon(updatedata, SetCon.CreatConnect(), PerVCC.getId(), PerVCC.getView());
+            fxmlLoader.setController(ControllerUpCr);
+        }else {
+            ContCreatUpdateViewComCon ControllerUpCr = new ContCreatUpdateViewComCon(updatedata, SetCon.CreatConnect(), 0,"");
+            fxmlLoader.setController(ControllerUpCr);
+        }
+
+        Parent rootS = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(rootS));
+        stage.setOnHiding(event -> {
+            try {
+                refrashTableView();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+        stage.setOnCloseRequest(event -> {
+            try {
+                refrashTableView();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+        stage.show();
     }
 
     public ControllerViewComongConsts(SettingConnectSQL SetCon){
--- src/Sempel/FormDirectory/DirectoruViewComingCosts/CreatUpdateViewComingCosts/ContCreatUpdateViewComCon.java
+++ src/Sempel/FormDirectory/DirectoruViewComingCosts/CreatUpdateViewComingCosts/ContCreatUpdateViewComCon.java
@@ -1,4 +1,112 @@
 package Sempel.FormDirectory.DirectoruViewComingCosts.CreatUpdateViewComingCosts;
 
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.XPath;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import sql.Connect;
+import sql.SelectPost;
+import sql.SettingConnectSQL;
+
 public class ContCreatUpdateViewComCon {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField EditID;
+
+    @FXML
+    private TextField Editname;
+
+    @FXML
+    private Label LabelID;
+
+    @FXML
+    private Label LabelName;
+
+    @FXML
+    private Button ButtonSave;
+
+    private Integer idView;
+    private String nameView;
+    private boolean updateViewComCon;
+    private Connection ConSQL;
+
+    @FXML
+    void initialize() {
+
+        EditID.setText(idView.toString());
+        Editname.setText(nameView);
+
+        ButtonSave.setOnAction(event -> {
+            if (updateViewComCon){
+                String sqlText = "UPDATE public.\"ViewComingConsumption\" " +
+                        " SET name_viewcc='"+Editname.getText()+"' " +
+                        " WHERE id_viewcc="+EditID.getText()+" ;";
+                try {
+                    requestSQL(sqlText);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                String sqlText = "INSERT INTO public.\"ViewComingConsumption\"(" +
+                        " name_viewcc, deleted_viewcc) " +
+                        " VALUES ('"+ Editname.getText() +"', false);";
+                try {
+                    requestSQL(sqlText);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private void requestSQL(String sqlText) throws SQLException {
+        SelectPost selPost = new SelectPost();
+        boolean rezRequest = selPost.UpdateCreatTable(ConSQL,sqlText);
+
+        if (rezRequest == false){
+            Stage stafeForm = (Stage) ButtonSave.getScene().getWindow();
+            stafeForm.close();
+        } else {
+            Alert alertForm = new Alert(Alert.AlertType.ERROR);
+            alertForm.setTitle("ERROR");
+            if (updateViewComCon) {
+                alertForm.setHeaderText("Error update");
+            } else {
+                alertForm.setHeaderText("Error Creat");
+            }
+            alertForm.show();
+        }
+
+    }
+
+    public ContCreatUpdateViewComCon(boolean updateViewComCon, Connection ConSQL, Integer idView, String nameView){
+
+        if (updateViewComCon){
+            this.idView = idView;
+            this.nameView = nameView;
+            this.ConSQL = ConSQL;
+            this.updateViewComCon = updateViewComCon;
+        } else {
+            this.idView = 0;
+            this.nameView = "";
+            this.ConSQL = ConSQL;
+            this.updateViewComCon = updateViewComCon;
+        }
+
+    }
 }
