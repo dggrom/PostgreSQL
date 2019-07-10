2611f2f79d5e50c201687bc0b5f4e8d6975a3281
dggrom <dg-grom@mail.ru>
2019-07-11 00:05:48 +0300

    Creat two directoru form and controller

---
+++ src/Sempel/FormDirectory/DirectoruViewDebt/ControllerViewDebt.java
@@ -0,0 +1,44 @@
+package Sempel.FormDirectory.DirectoruViewDebt;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ControllerViewDebt {
+
+        @FXML
+        private ResourceBundle resources;
+
+        @FXML
+        private URL location;
+
+        @FXML
+        private Button ButtonRefresh;
+
+        @FXML
+        private Button ButtonDeleted;
+
+        @FXML
+        private Button ButtonCreat;
+
+        @FXML
+        private TableView<?> TableViewComCon;
+
+        @FXML
+        private TableColumn<?, ?> TableColumnsID;
+
+        @FXML
+        private TableColumn<?, ?> TableColumnsView;
+
+        @FXML
+        private TableColumn<?, ?> TableColumnsDeleted;
+
+        @FXML
+        void initialize() {
+
+        }
+    }
