dcadfabe2b790496c2f7caa7fff91a37ad6437ef
dggrom <45589263+dggrom@users.noreply.github.com>
2020-06-29 19:39:36 +0300

    Update PersenNomenclatura.java

--- src/Sempel/FormDirectory/DirectoruNomenclature/PersenNomenclatura.java
+++ src/Sempel/FormDirectory/DirectoruNomenclature/PersenNomenclatura.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import sql.SelectPost;
 import sql.SettingConnectSQL;
